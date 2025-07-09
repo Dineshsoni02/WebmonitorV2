c37eefeb776ca678e6c6d7e717023f90fe8e3f45
Dinesh Kumar <dineshsoni8569@gmail.com>
2025-07-09 21:51:59 +0530

    Schema init

---
+++ backend/app/website/WebsiteScheam.js
@@ -0,0 +1,25 @@
+import mongoose, { Model, model, modelNames } from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    token: {
+      accessToken: String,
+      refreshToken: String,
+    },
+  },
+  { timestamps: true }
+);
+
