bea17b8e66726398c2371f387649cf2ebb4abcb3
Dinesh Kumar <dineshsoni8569@gmail.com>
2025-07-16 21:52:45 +0530

    restructuring

---
+++ backend/app/constants/responseMessages.js
@@ -0,0 +1,1 @@
+const FIELDS_REQUIRED = 
--- backend/app/utils/token.js
+++ backend/app/utils/token.js
@@ -3,3 +3,10 @@ import jwt from "jsonwebtoken";
 const secretKey = process.env.SECRET_KEY || "fallback_secret";
 
 export const getExpiry = (days) => Math.floor(Date.now() / 1000 + days * 86400);
+
+export const generateToken = (data, expiry) => {
+  if (!expiry) expiry = getExpiry(1);
+
+  const token = jwt.sign({ data, secretKey }, expiry);
+  return token;
+};
