2d6802426d5faaa19d7488bf41bea5390d4a526e
Dinesh Kumar <dineshsoni8569@gmail.com>
2025-07-16 22:04:31 +0530

    token functions

--- backend/app/constants/responseMessages.js
+++ backend/app/constants/responseMessages.js
@@ -1,1 +1,10 @@
-const FIELDS_REQUIRED = 
+export const messages = {
+  FIELDS_REQUIRED: "All fields are mandatory!",
+  EMAIL_INVALID: "E-mail is invalid",
+  USER_EXISTS: "User already exists with this email",
+  USER_NOT_FOUND: "User not found",
+  PASSWORD_INCORRECT: "Credentials don't match",
+  SIGNUP_ERROR: "Error in user signup",
+  SIGNUP_SUCCESS: "User successfully created!",
+  LOGIN_SUCCESS: "User successfully logged in",
+};
--- backend/app/utils/token.js
+++ backend/app/utils/token.js
@@ -7,6 +7,18 @@ export const getExpiry = (days) => Math.floor(Date.now() / 1000 + days * 86400);
 export const generateToken = (data, expiry) => {
   if (!expiry) expiry = getExpiry(1);
 
-  const token = jwt.sign({ data, secretKey }, expiry);
+  const token = jwt.sign({ data, expiry }, secretKey);
   return token;
+};
+
+export const decodeToken = (token) => {
+  let data;
+
+  try {
+    data = jwt.verify(token, secretKey);
+  } catch (error) {
+    console.log("error verifying token", error);
+  }
+
+  return data;
 };
