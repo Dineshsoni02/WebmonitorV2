88cb9e86b3965957ce1c78ab63907067a4e9801d
Dinesh Kumar <dineshsoni8569@gmail.com>
2025-08-03 22:45:34 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-400">
