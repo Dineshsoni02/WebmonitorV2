d1744c94c6f4ecd88986c94cd89035c5182c7fa9
Dinesh Kumar <dineshsoni8569@gmail.com>
2025-08-08 21:24:41 +0530

    dialog improvement

--- frontend/src/components/BodyComponent.jsx
+++ frontend/src/components/BodyComponent.jsx
@@ -12,6 +12,7 @@ import {
   TrendingUp,
   TriangleAlert,
   Globe,
+  X,  
 } from "lucide-react";
 
 const NavigationBar = () => {
@@ -420,7 +421,7 @@ const DashboardSection = () => {
 
 const DialogBox = () => {
   return (
-    <div className="max-w-sm p-8 mx-auto fixed top-0 left-0 right-0 z-50 bg-gradient-to-r from-gray-900/95 to-gray-800/95 backdrop-blur-md border-b border-gray-700/50 text-white rounded-xl my-[15%]">
+    <div className="max-w-sm p-8 mx-auto fixed inset-0 z-50 bg-gradient-to-r from-gray-900/95 to-gray-800/95 backdrop-blur-md border-b border-gray-700/50 text-white rounded-xl my-[15%] h-fit">
       <div className="flex items-center gap-2 mb-4">
         <Globe className="w-6 h-6" />
 
@@ -429,22 +430,28 @@ const DialogBox = () => {
         </h2>
       </div>
       <button className=" text-white  rounded-lg font-medium transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:shadow-cyan-500/25 cursor-pointer absolute top-2 right-2">
-        X
+        <X className="w-6 h-6" />
       </button>
 
       <form className="flex flex-col gap-4">
-        <input
-          type="text"
-          placeholder="Enter Website URL"
-          required
-          className="bg-transparent border border-gray-700/50 hover:border-gray-600/80 p-2 rounded-xl shadow-md hover:shadow-[0px_0px_28px_2px] hover:shadow-cyan-500/25 transition-all duration-300"
-        />
-        <input
-          type="text"
-          placeholder="Enter Website Name"
-          required
-          className="bg-transparent border border-gray-700/50 hover:border-gray-600/80 p-2 rounded-xl shadow-md hover:shadow-[0px_0px_28px_2px] hover:shadow-cyan-500/25 transition-all duration-300"
-        />
+        <div className="flex flex-col gap-2">
+          <label htmlFor="url" className="text-white">Enter Website URL *</label>
+          <input
+            type="text"
+            placeholder="Enter Website URL"
+            required
+            className="bg-transparent border border-gray-700 hover:border-gray-600/80 p-2 rounded-lg shadow-md  hover:shadow-cyan-500/10 transition-all duration-300"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="name" className="text-white">Enter Website Name *</label>
+          <input
+            type="text"
+            placeholder="Enter Website Name"
+            required
+            className="bg-transparent border border-gray-700 hover:border-gray-600/80 p-2 rounded-lg shadow-md hover:shadow-cyan-500/10 transition-all duration-300"
+          />
+        </div>
         <button className="bg-gradient-to-r from-blue-600 to-cyan-600 hover:from-blue-700 hover:to-cyan-700 text-white px-6 py-2 rounded-lg font-medium transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:shadow-cyan-500/25 cursor-pointer">
           Submit
         </button>
