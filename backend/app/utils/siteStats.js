6e106daeb08ddd1114276dd13ba11906c582f3e2
Dinesh Kumar <dineshsoni8569@gmail.com>
2025-08-14 21:12:42 +0530

    function improvement

--- backend/app/utils/siteStats.js
+++ backend/app/utils/siteStats.js
@@ -10,7 +10,7 @@ export const isSiteActive = async (url) => {
   if (!url) return false;
 
   const res = await axios.get(url).catch((err) => void err);
-  if (!res || !res.status == 200) return false;
+  if (!res || res.status !== 200) return false;
   return true;
 };
 
